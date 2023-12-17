41c73e2a333873d89d989b83034d06f38c8e8ab4
Kortzey <135766095+Kortzey@users.noreply.github.com>
2023-12-17 08:17:47 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const home = document.getElementById('home');
+    home.onclick = () => {
+    window.location.href = 'index.html';
+}
+
+const menu = document.getElementById('menu');
+    menu.onclick = () => {
+    window.location.href = 'menu.html';
+}
+
+const joinOurTeam = document.getElementById('joinOurTeam');
+    joinOurTeam.onclick = () => {
+    window.location.href = 'joinOurTeam.html';
+}
